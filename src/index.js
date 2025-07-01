b596b5713dfa9c1d6c59126024c74c2cc2cd56e6
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-07-01 20:58:21 +0200

    Move game logic to gameController

--- src/index.js
+++ src/index.js
@@ -1,31 +1,8 @@
-import { generateEnemyBoard, generatePlayerBoard } from "./modules/domUtils";
-import { Player } from "./modules/player";
-import { Ship } from "./modules/ship";
 import "./css/styles.css";
 import "./css/modern-normalize.css";
+import { Game } from "./modules/gameController";
+import { listenForAttacks } from "./modules/domUtils";
 
-const player1 = new Player();
-const player2 = new Player("computer");
-
-const ship1 = new Ship(2);
-const ship2 = new Ship(1);
-const ship3 = new Ship(5);
-const ship4 = new Ship(2);
-
-const ship5 = new Ship(2);
-const ship6 = new Ship(1);
-const ship7 = new Ship(5);
-const ship8 = new Ship(2);
-
-player1.gameboard.placeShip(ship1, 8, 0, "vertical");
-player1.gameboard.placeShip(ship2, 1, 3);
-player1.gameboard.placeShip(ship3, 4, 4);
-player1.gameboard.placeShip(ship4, 8, 6, "vertical");
-
-player2.gameboard.placeShip(ship5, 1, 0, "vertical");
-player2.gameboard.placeShip(ship6, 0, 3);
-player2.gameboard.placeShip(ship7, 3, 2);
-player2.gameboard.placeShip(ship8, 7, 6, "vertical");
-
-generatePlayerBoard(player1);
-generateEnemyBoard(player2);
+const game = new Game();
+game.start();
+listenForAttacks(game);
