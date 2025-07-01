1ad78c09b25b9fd00a693a484b39df8365fafe23
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-07-01 10:15:16 +0200

    Split generateBoard in two functions

--- src/modules/domUtils.js
+++ src/modules/domUtils.js
@@ -1,8 +1,13 @@
 const container = document.querySelector(".container");
 
-export function generateBoard(player) {
+export function generatePlayerBoard(player) {
+  const title = document.createElement("h2");
+  title.textContent = "Player";
+  container.appendChild(title);
+
   const board = document.createElement("div");
   board.classList.add("board");
+  board.classList.add("player");
   board.setAttribute("data", `"player": ${player.type}`);
 
   container.appendChild(board);
@@ -19,6 +24,29 @@ export function generateBoard(player) {
       if (fleetCell) {
         cellDiv.classList.add("ship");
       }
+    }
+  }
+}
+
+export function generateEnemyBoard(player) {
+  const title = document.createElement("h2");
+  title.textContent = "Enemy";
+  container.appendChild(title);
+
+  const board = document.createElement("div");
+  board.classList.add("board");
+  board.classList.add("enemy");
+  board.setAttribute("data", `"player": ${player.type}`);
+
+  container.appendChild(board);
+
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      const cellDiv = document.createElement("div");
+
+      cellDiv.classList.add("cell");
+      cellDiv.setAttribute("data", `"coordinates": ${i},${j}`);
+      board.appendChild(cellDiv);
     }
   }
 }
